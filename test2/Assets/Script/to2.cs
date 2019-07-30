0faa1d1553f031d993b4ce060e933c5d0042f558
zcbGC <zcbGC@users.noreply.github.com>
2019-07-30 09:17:11 +0800

    7-30-9

--- test2/Assets/Script/to2.cs
+++ test2/Assets/Script/to2.cs
@@ -6,9 +6,13 @@ using UnityEngine.SceneManagement;
 public class to2 : ScriptableObject
 {
     // Start is called before the first frame update
+    private gamemanager gamemanager;
+    private int charm;
+    private int money;
+    private int hungar;
     void Start()
     {
-
+        gamemanager = GameObject.Find("GameManager").GetComponent<gamemanager>();
     }
 
     // Update is called once per frame
@@ -16,9 +20,28 @@ public class to2 : ScriptableObject
     {
 
     }
-
-    public void ChangeScence()
+    public void seth(int x)
     {
-        SceneManager.LoadScene(2);
+        gamemanager.hungar = x;
+    }
+    public void addh(int x)
+    {
+        gamemanager.hungar += x;
+    }
+    public void setm(int x)
+    {
+        gamemanager.money = x;
+    }
+    public void addm(int x)
+    {
+        gamemanager.money += x;
+    }
+    public void setc(int x)
+    {
+        gamemanager.charm = x;
+    }
+    public void addc(int x)
+    {
+        gamemanager.charm += x;
     }
 }
--- test2/Assets/gamemanager.cs
+++ test2/Assets/gamemanager.cs
@@ -8,6 +8,10 @@ public class gamemanager : MonoBehaviour
     // Start is called before the first frame update
     public static gamemanager gameInstance;
     private SceneFadeInOut fadescene;
+    public int charm;
+    public int money;
+    public int hungar; 
+    public Dictionary<string,int> item;
     void Awake()
     {
         if (gameInstance == null)
@@ -23,7 +27,13 @@ public class gamemanager : MonoBehaviour
 
     void Start()
     {
-        
+        item.Add("笔", 0);
+        item.Add("手电筒", 0);
+        item.Add("绳子", 0);
+        item.Add("钥匙", 0);
+        item.Add("钱包", 0);
+        item.Add("手机", 0);
+        item.Add("晕车药", 0);
     }
 
     // Update is called once per frame
@@ -38,5 +48,29 @@ public class gamemanager : MonoBehaviour
         fadescene = GameObject.Find("RawImage").GetComponent<SceneFadeInOut>();
         fadescene.scenenum = x;
         fadescene.sceneEnding = true;
+    }
+    public void seth(int x)
+    {
+        hungar = x;
+    }
+    public void addh(int x)
+    {
+        hungar += x;
+    }
+    public void setm(int x)
+    {
+        money = x;
+    }
+    public void addm(int x)
+    {
+        money += x;
+    }
+    public void setc(int x)
+    {
+        charm = x;
+    }
+    public void addc(int x)
+    {
+        charm += x;
     }
 }
