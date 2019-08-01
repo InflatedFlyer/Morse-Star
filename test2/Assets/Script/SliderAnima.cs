07ca26d4a6965e7e0e680fb6fd7e151595aa01a6
zcbGC <zcbGC@users.noreply.github.com>
2019-08-01 23:14:23 +0800

    8-1-23

--- test2/Assets/Script/SliderAnima.cs
+++ test2/Assets/Script/SliderAnima.cs
@@ -8,11 +8,13 @@ public class SliderAnima : MonoBehaviour
     public int fullValue = 15;  //最大数值
     public int nowValue = 6;    //当前数值
     public float Percent = 0.1f;    //单次插值从from到to的百分比
+    public float Multiple = 0.025f;   //颜色插值倍数
     public float Boundary = 0.005f;     //插值最小精度
 
     private float aimValue;   //目标缩放
     private bool flag;  //是否执行插值动画
-
+    private bool isplus; //加减正负号
+    private Transform pic; //子物体中上覆盖图片
 
     private
 
@@ -21,6 +23,10 @@ public class SliderAnima : MonoBehaviour
     void Start()
     {
         this.GetComponent<Slider>().value = ((float)nowValue / fullValue);
+
+        Transform obj = transform.Find("Fill");
+        pic = obj.Find("Image");
+
     }
 
     // Update is called once per frame
@@ -29,10 +35,19 @@ public class SliderAnima : MonoBehaviour
         if (flag)   //插值动画
         {
             this.GetComponent<Slider>().value = Mathf.Lerp(this.GetComponent<Slider>().value, aimValue, Percent);
-            if(Mathf.Abs(this.GetComponent<Slider>().value-aimValue) < Boundary)
+            if (isplus)
+                pic.GetComponent<Image>().color = Color.Lerp(new Color(0.2f, 1, 0.2f, 1), Color.white, Multiple = Multiple * (1 + Multiple));
+            else
+                pic.GetComponent<Image>().color = Color.Lerp(new Color(1, 0.2f, 0.2f, 1), Color.white, Multiple = Multiple * (1 + Multiple));
+
+            if (Mathf.Abs(this.GetComponent<Slider>().value-aimValue) < Boundary)
             {
                 this.GetComponent<Slider>().value = aimValue;
-                flag = false;
+                if (Multiple >= 1)
+                {
+                    pic.GetComponent<Image>().color = Color.white;
+                    flag = false;
+                }
             }
         }
     }
@@ -42,6 +57,10 @@ public class SliderAnima : MonoBehaviour
         nowValue += p;
         aimValue = 1 * ((float)nowValue / fullValue);   //数值转换为缩放
         flag = true;
+        if (p > 0)
+            isplus = true;
+        else
+            isplus = false;
     }
 
     public void AddValue(string p)
@@ -49,5 +68,9 @@ public class SliderAnima : MonoBehaviour
         nowValue += int.Parse(p);
         aimValue = 1 * ((float)nowValue / fullValue);   //数值转换为缩放
         flag = true;
+        if (int.Parse(p) > 0)
+            isplus = true;
+        else
+            isplus = false;
     }
 }
--- test2/Assets/gamemanager.cs
+++ test2/Assets/gamemanager.cs
@@ -8,10 +8,11 @@ public class gamemanager : MonoBehaviour
     // Start is called before the first frame update
     public static gamemanager gameInstance;
     private SceneFadeInOut fadescene;
-    public int Charm;
-    public int Money;
-    public int Hungar;
-    public int Word;
+    public playbox playbox;
+    [HideInInspector] public int Charm;
+    [HideInInspector] public int Money;
+    [HideInInspector] public int Hungar;
+    [HideInInspector] public int Word;
     //[HideInInspector] public Dictionary<int, string> order;
     [HideInInspector] public Dictionary<string,int> itemd;
     [HideInInspector] public Dictionary<string, string> itemword;
@@ -41,10 +42,22 @@ public class gamemanager : MonoBehaviour
         itemd.Add("手机", 0);
         itemd.Add("钱包", 1);
         itemd.Add("戒指", 2);
+        itemd.Add("吐司", 3);
+        itemd.Add("手杖", 4);
+        itemd.Add("巧克力", 5);
+        itemd.Add("小玩偶", 6);
+        itemd.Add("绳子", 7);
+        itemd.Add("首饰盒", 8);
         itemword = new Dictionary<string, string>();
         itemword.Add("手机", "这是一个手机");
         itemword.Add("钱包", "这是一个钱包");
         itemword.Add("戒指", "这是一个戒指");
+        itemword.Add("吐司", "常见的甜点，在家里放了一段时间了");
+        itemword.Add("手杖", "");
+        itemword.Add("巧克力", "");
+        itemword.Add("小玩偶", "");
+        itemword.Add("绳子", "");
+        itemword.Add("首饰盒", "");
         //item.Add("钥匙", 0);
         //item.Add("笔", 0);
         //item.Add("手机", 0);
@@ -101,11 +114,13 @@ public class gamemanager : MonoBehaviour
         int s;
         s = itemd[x];
         itemflag[s] = true;
+        playbox.sort();
     }
     public void lossitem(string x)
     {
         int s;
         s = itemd[x];
         itemflag[s] = false;
+        playbox.sort();
     }
 }
